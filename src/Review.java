24648d611349fa31df61b0abb0a4afed5ae8ca19
tj22aau <tj22aau@herts.ac.uk>
2023-04-24 01:41:51 +0100

    Review.java updated now

--- src/Review.java
+++ src/Review.java
@@ -1,21 +1,26 @@
 public class Review {
     private Customer customer;
-    private String review;
+    private Lesson lesson;
+    private String reviewText;
     private int rating;
 
-    public Review(Customer customer, String review, int rating) {
+    public Review(Customer customer, Lesson lesson, String reviewText, int rating) {
         this.customer = customer;
-        this.review = review;
+        this.lesson = lesson;
+        this.reviewText = reviewText;
         this.rating = rating;
     }
 
-    // Getters and setters
     public Customer getCustomer() {
         return customer;
     }
 
-    public String getReview() {
-        return review;
+    public Lesson getLesson() {
+        return lesson;
+    }
+
+    public String getReviewText() {
+        return reviewText;
     }
 
     public int getRating() {
